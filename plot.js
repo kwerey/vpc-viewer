0259f182f45f35517df54aafb4efe418c1c7dab7
Nikki laptop <nikobelia@gmail.com>
2016-12-10 22:58:58 +0000

    Loads JSON from a file

--- plot.js
+++ plot.js
@@ -1,8 +1,9 @@
+
 // preparing data for this https://github.com/d3/d3-hierarchy
 
 // vpcs.json should be the output of `aws ec2 describe-vpcs`
 // subnets.json should be the output of `aws ec2 describe-subnets`
-tree = parse('data/vpcs.json', 'data/subnets.json')
+var tree = parse('data/vpcs.json', 'data/subnets.json')
 
 function loadJSON(file, callback) {   
 
@@ -67,39 +68,15 @@ function drawGraph(tree) {
   console.log("Plotting a graph with the contents of: ")
   console.log(tree)
 
-  var svg = d3.select("#canvas"),
-      margin = 20,
-      diameter = +svg.attr("width"),
-      g = svg.append("g").attr("transform", "translate(2,2)");
+  vpcs = tree['children'];
 
-    var format = d3.format(",d");
-
-    var color = d3.scaleSequential(d3.interpolateMagma)
-        .domain([-4, 4]);
-
-    var pack = d3.pack()
-        .size([diameter - 2, diameter - 2])
-        .padding(2);
-
-    // reads in the hierarchy the script above made.
-    vpcData = d3.hierarchy(tree)
-      .sum(function(d) { return d.size })
-      .sort(function(a, b) { return b.value - a.value; });
-
-    var node = g.selectAll(".node")
-      .data(pack(vpcData).descendants())
-      .enter() // d3 magic ensures an svg group exists for vpcData node. 
-      .append("g")
-        .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-    node.append("title")
-        .text(function(d) { return d.data.name });
-
-    node.append("circle")
-        .attr("r", function(d) { return d.value });
-
-    node.append("title")
-        .text(function(d) { return d.data.name });
+  d3.select("#canvas").selectAll("div")
+  .data(vpcs)
+  .enter()
+  .append("div")
+  .attr("class", "vpc")
+  .attr("id", function(d) { return d.VpcId; })
+  .attr("title", function(d) { return d.CidrBlock; })
+  .html(function(d) { return d.Tags[0].Value })
 
 }
