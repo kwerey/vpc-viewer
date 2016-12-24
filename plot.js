8e0d81bc996a18771f13f627dfe75cb9f8e781d0
nbailey <nbailey@illumina.com>
2016-12-24 17:01:54 +0000

    Better spacing/svg coords

--- plot.js
+++ plot.js
@@ -27,6 +27,8 @@ function loadJson(file, callback) {
 function parseJson(vpcs, subnets) {
   Promise.all([loadJson(vpcs), loadJson(subnets)]).then(function(responses) {
     // responses contains the parsed JSON objects in the order of requests
+
+    // I should verify that they're valid json before doing the next bit.
     var parsedJson = toHierarchy(responses[0].Vpcs, responses[1].Subnets);
   }).catch(function(error) {
       // do error processing here if any promise was rejected
@@ -34,9 +36,10 @@ function parseJson(vpcs, subnets) {
 }
 
 function toHierarchy(vpcs, subnets) {
-  console.log("To hierarcy:")
-  console.log(vpcs)
-  console.log(subnets)
+  console.log("To hierarchy:");
+  // console.log(vpcs);
+  // console.log(subnets);
+
   // Make root object
   var root = {
     name: 'ROOT',
@@ -62,7 +65,7 @@ function toHierarchy(vpcs, subnets) {
       }
     }
   }
-  console.log("Now we have a hierarchy. draw a graph!")
+  console.log("Now we have a hierarchy. draw a graph!");
   drawGraph(root);
 }
 
@@ -73,34 +76,47 @@ function drawGraph(tree) {
   // thanks SO - https://stackoverflow.com/questions/18147915/get-width-height-of-svg-element
   var canvas = document.getElementById('canvas').getBoundingClientRect();
   var width = canvas.width;
+  var height = canvas.height;
   var vpcs = tree.children;
-  var vpcWidth = ( width / vpcs.length ) - 15; // or something along those lines
+  var vpcWidth = ( width / vpcs.length );
+  // i think this'll save a third of the canvas for padding/edges.
 
-  console.log("each vpc should be " + vpcWidth + "px wide")
+  console.log("The max size to allocate to each vpc is " + vpcWidth + "px");
 
-  // d3.select("#canvas").selectAll(".vpc")
   var vpcNode = d3.select("#canvas").selectAll(".vpc")
   .data(vpcs)
   .enter()
   .append("g")
   .attr("class", "vpc")
-  .attr("id", function(d) { return d.meta.VpcId; })
-  .attr("title", function(d) { return d.meta.CidrBlock; })
+  .attr("id", function(d) {
+    return d.meta.VpcId;
+  })
+  .attr("title", function(d) {
+    return d.meta.CidrBlock;
+  })
   // do something more robust in the line below to get name - "if a tag with key Name exists, then..."
 
   vpcNode.append("text")
+  .attr("class", "vpc-name")
   // per https://www.dashingd3js.com/svg-text-element
-  .html(function(d) { if (d.meta.Tags != null) { return d.meta.Tags[0].Value } else { return "unnamed vpc" } })
-  .attr("text-anchor", "middle")
-  .attr("x", function(d) { return (vpcs.indexOf(d) - 1) * (vpcWidth) }) // ugly but approximately functional!
-  .attr("y", -40)
+  .html(function(d) {
+    if (d.meta.Tags != null) {
+      return d.meta.Tags[0].Value
+    } else {
+      return "unnamed vpc"
+    }
+  })
+  .attr("x", function(d) {
+    return ((vpcs.indexOf(d) * vpcWidth) + 30)
+  }) // ugly but approximately functional!
+  .attr("y", 40)
 
   vpcNode.append("rect")
-  .attr("width", vpcWidth / 2)
-  .attr("height", vpcWidth / 2)
-  .attr("x", function(d) { return (vpcs.indexOf(d) - 1) * (vpcWidth) }) // ugly but approximately functional!
-  .attr("y", -40)
-
-//  .attr("transform", function(d) { return "translate(" + d.vpcWidth + "," + d.vpcWidth + ")" })
+  .attr("width", vpcWidth - 40)
+  .attr("height", height - 40)
+  .attr("x", function(d) {
+    return (vpcs.indexOf(d) * (vpcWidth) + 10)
+  }) // ugly but approximately functional!
+  .attr("y", 20)
 
 }
